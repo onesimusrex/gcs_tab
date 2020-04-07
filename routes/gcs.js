58f2938c26d3aa9ab15cb39e90199a29c890b3cf
David <onesimusrex@gmail.com>
2020-04-07 15:12:36 -0400

    everything10

--- routes/gcs.js
+++ routes/gcs.js
@@ -7,15 +7,19 @@ router.post('/', function(req, res, next) {
     var _maxResults = 20;
     // console.log(req.body.query);
     _query = req.body.query;
+    _queryType = req.body.queryType;
     _gcsResults = {"results":[]};
-    getCustomSearch(_query, _gcsResults, res, -1, _maxResults);
+    getCustomSearch(_query, _gcsResults, res, -1, _maxResults, _queryType);
 });
 
-function getCustomSearch(_query, _gcsResults, res, _start, _maxResults){
+function getCustomSearch(_query, _gcsResults, res, _start, _maxResults, _queryType){
+    if (_queryType == 'ifs'){
+        _cx = process.env.TYNDALL_GCS_CX_IFS;
+    } else _cx = process.env.TYNDALL_GCS_CX_MAIN;
     if (_start > 0){
-        queryString = 'https://www.googleapis.com/customsearch/v1?key='+process.env.TYNDALL_GCS_KEY_IFS+'&cx=006933437979345530940:kp6pe1dh7oh&q='+_query+'&start='+(_start-1);
+        queryString = 'https://www.googleapis.com/customsearch/v1?key='+process.env.TYNDALL_GCS_KEY_IFS+'&cx='+_cx+'&q='+_query+'&start='+(_start-1);
     } else {
-        queryString = 'https://www.googleapis.com/customsearch/v1?key='+process.env.TYNDALL_GCS_KEY_IFS+'&cx=006933437979345530940:kp6pe1dh7oh&q='+_query;
+        queryString = 'https://www.googleapis.com/customsearch/v1?key='+process.env.TYNDALL_GCS_KEY_IFS+'&cx='+_cx+'&q='+_query;
     }
     request
         .get(queryString)
@@ -27,7 +31,7 @@ function getCustomSearch(_query, _gcsResults, res, _start, _maxResults){
             if(typeof data.body.queries.nextPage !== 'undefined' && _gcsResults.results[0].items.length < _maxResults){
                 // console.log('rerunning');
                 _start = data.body.queries.nextPage[0].startIndex;
-                getCustomSearch(_query, _gcsResults, res, _start, _maxResults);
+                getCustomSearch(_query, _gcsResults, res, _start, _maxResults,_queryType);
             } else res.send(_gcsResults.results[0]);
         });
     
